87894c22fc430b24e8322fb31bb0d35a598be8db
AquIce <timeo.perrinjaquet@gmail.com>
2024-05-24 17:12:32 +0200

    Fixed Environment.set

--- srcpp/include/AquIce/Entropia/env.hpp
+++ srcpp/include/AquIce/Entropia/env.hpp
@@ -23,8 +23,8 @@ namespace ent {
 				return this->values.find(key) != this->values.end();
 			}
 			RuntimeValue* set(std::string key, RuntimeValue* value) {
-				if(this->has(key)) {
-					throw (Error(ErrorType::RUNTIME_ERROR, "Variable already defined: " + key)).error();
+				if(!this->has(key)) {
+					throw (Error(ErrorType::RUNTIME_ERROR, "Variable is not defined: " + key)).error();
 				}
 				this->values[key] = value;
 				return value;
